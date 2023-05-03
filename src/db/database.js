e541349dda2f2da81ff38d35f590d12be1298c03
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-05-03 17:09:19 +0100

    Update db

--- src/db/database.js
+++ src/db/database.js
@@ -2,9 +2,6 @@ require("dotenv").config();
 
 module.exports = {
   database: "open_fabric",
-  host: process.env.DB_PROD_HOST,
-  username: process.env.DB_PROD_USER,
-  password: process.env.DB_PROD_PASS,
   url: process.env.DB_PROD_URL,
   dialect: "postgres",
 };
