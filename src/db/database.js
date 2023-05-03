6897ad4b7d02a82dcadba7025ed09f880f7d9e0c
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-05-03 06:01:34 +0100

    Update codebase

---
+++ src/db/database.js
@@ -0,0 +1,28 @@
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "",
+//     DB: "scelloo_task",
+//     dialect: "mysql",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     }
+//   };
+
+//heroku config
+module.exports = {
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "",
+  DB: "open-fabric",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
