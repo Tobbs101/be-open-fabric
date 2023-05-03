fca68811de868aaae51f441f3224767b72862f34
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-05-03 06:03:13 +0100

    Update database.js

--- src/db/database.js
+++ src/db/database.js
@@ -1,18 +1,3 @@
-// module.exports = {
-//     HOST: "localhost",
-//     USER: "root",
-//     PASSWORD: "",
-//     DB: "scelloo_task",
-//     dialect: "mysql",
-//     pool: {
-//       max: 5,
-//       min: 0,
-//       acquire: 30000,
-//       idle: 10000
-//     }
-//   };
-
-//heroku config
 module.exports = {
   HOST: "localhost",
   USER: "root",
